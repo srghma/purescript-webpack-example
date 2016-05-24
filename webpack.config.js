63e4c3603b3bb3bd75f67b653258ccbfa6adea9b
eric thul <thul.eric@gmail.com>
2016-05-23 21:21:44 -0400

    Updates for purs-loader@next

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var PurescriptWebpackPlugin = require('purescript-webpack-plugin');
-
 var src = ['bower_components/purescript-*/src/**/*.purs', 'src/Example/**/*.purs'];
 
 var ffi = ['bower_components/purescript-*/src/**/*.js', 'src/Example/**/*.js'];
@@ -10,16 +8,6 @@ var modulesDirectories = [
   'node_modules',
   'bower_components'
 ];
-
-var purescriptWebpackPlugin = new PurescriptWebpackPlugin({
-  src: src,
-  ffi: ffi,
-  bundle: false,
-  psc: 'psa',
-  pscArgs: {
-    sourceMaps: true
-  }
-});
 
 var config
   = { entry: './src/entry'
@@ -35,6 +23,12 @@ var config
               }
     , module: { loaders: [ { test: /\.purs$/
                            , loader: 'purs-loader'
+                           , query: { src: src
+                                    , ffi: ffi
+                                    , bundle: false
+                                    , psc: 'psa'
+                                    , pscArgs: { sourceMaps: true }
+                                    }
                            }
                          , { test: /\.js$/
                            , loader: 'source-map-loader'
@@ -43,9 +37,8 @@ var config
                          ]
               }
     , resolve: { modulesDirectories: modulesDirectories
-               , extensions: [ '', '.js', '.purs']
+               , extensions: [ '', '.purs', '.js']
                }
-    , plugins: [ purescriptWebpackPlugin ]
     }
     ;
 
