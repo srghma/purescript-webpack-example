9ed5df94b8103c207925540f0bacddf7f3c4c3ae
eric thul <thul.eric@gmail.com>
2016-11-19 19:28:05 -0500

    Example geared toward fast rebuilds

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# purescript-webpack-example
+# purescript-webpack-example (fast rebuilds)
 
-Example project using PureScript with webpack.
+Example project using PureScript with webpack that illustrates a
+configuration geared towards fast rebuilds.
 
-```bash
-bower install
+The main parts to the configuration are enabling `pscIde` in the
+`purs-loader` configuration, and using webpack's
+[DllPlugin](https://github.com/webpack/docs/wiki/list-of-plugins#dllplugin)
+to delegate to pre-build external modules.
 
-npm install
-
-npm run webpack
-
-npm start
-```
-
-To test this in the browser with the webpack-dev-server.
+To get the example up and running, please run the following commands.
 
 ```bash
+npm install # yarn may also be used for this step
+
 bower install
 
-npm install
+npm run webpack:dll
 
-# http://localhost:4008/
 npm run webpack:server
 ```
+
+The example will then be available in browser at [http://localhost:4008/](http://localhost:4008/).
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,50 @@
 'use strict';
 
-var config
-  = { entry: './src/Example.purs'
-    , debug: true
-    , devtool: 'source-map'
-    , devServer: { contentBase: '.'
-                 , port: 4008
-                 , stats: 'errors-only'
-                 }
-    , output: { path: __dirname
-              , pathinfo: true
-              , filename: 'bundle.js'
-              }
-    , module: { loaders: [ { test: /\.purs$/
-                           , loader: 'purs-loader'
-                           , query: { src: [ 'bower_components/purescript-*/src/**/*.purs', 'src/**/*.purs' ]
-                                    , bundle: false
-                                    , psc: 'psa'
-                                    , pscArgs: { sourceMaps: true }
-                                    , pscIde: false
-                                    }
-                           }
-                         , { test: /\.js$/
-                           , loader: 'source-map-loader'
-                           , exclude: /node_modules|bower_components/
-                           }
-                         ]
-              }
-    , resolve: { modulesDirectories: [ 'node_modules', 'bower_components' ]
-               , extensions: [ '', '.purs', '.js']
-               }
-    }
-    ;
+var webpack = require('webpack');
 
-module.exports = config;
+module.exports = {
+  debug: true,
+
+  devtool: 'eval-source-map',
+
+  devServer: {
+    contentBase: '.',
+    port: 4008,
+    stats: { colors: true }
+  },
+
+  entry: './src/Example.purs',
+
+  output: {
+    path: __dirname,
+    pathinfo: true,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.purs$/,
+        loader: 'purs-loader',
+        query: {
+          src: [ 'bower_components/purescript-*/src/**/*.purs', 'src/**/*.purs' ],
+          bundle: false,
+          psc: 'psa',
+          pscIde: true
+        }
+      },
+    ]
+  },
+
+  resolve: {
+    modulesDirectories: [ 'node_modules', 'bower_components' ],
+    extensions: [ '', '.purs', '.js']
+  },
+
+  plugins: [
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require('./vendor-manifest.json')
+    })
+  ]
+};
